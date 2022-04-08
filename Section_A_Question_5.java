13c1e576468c87e6cd559b9193c479e18886f0f9
Jayasree0905 <103204432+Jayasree0905@users.noreply.github.com>
2022-04-08 22:10:59 +0530

    The given string has characters of the equal difference or not
    Check whether the given string has characters of equal difference or unequal difference.

--- Section_A_Question_5.java
+++ Section_A_Question_5.java
@@ -1,27 +1,29 @@
 /**
- * Check whether the given string has charaters of the equal differnce or not
+ * Check whether the given string has characters of the equal difference or not
  */
 public class Section_A_Question_5 {
-    public static void main(String args[]) {
-      equalDifference("abcdefg");
-      equalDifference("adxz");
-    }
-    public static void equalDifference(String input){
-         char[] cha = input.toCharArray(); // to convert string to char array
-      int len=cha.length;// length of the char array
-      Boolean isEqual = false;
-      //to compare next element with current element.
-      //if the difference is 1 then given string is "Equal" else "Unequal"
-      for(int i =0; i<len-1;i++){
-          if(cha[i+1] - cha[i] ==  1){
-              isEqual=true;
-          }else{
-              isEqual = false;
-          }
-      }
-      if(isEqual)
-      System.out.println("Equal difference");
-      else 
-      System.out.println("Unequal difference");
-    }
+	public static void main(String args[]) {
+		System.out.println("Output 1:");
+		equalDifference("abcdefg");
+		System.out.println("Output 2:");
+		equalDifference("adxz");
+	}
+	public static void equalDifference(String input) {
+		char[] cha = input.toCharArray(); // to convert string to char array
+		int len = cha.length;// length of the char array
+		Boolean isEqual = false;
+		// to compare next element with current element.
+		// if the difference is 1 then given string is "Equal" else "Unequal"
+		for (int i = 0; i < len - 1; i++) {
+			if (cha[i + 1] - cha[i] == 1) {
+				isEqual = true;
+			} else {
+				isEqual = false;
+			}
+		}
+		if (isEqual)
+			System.out.println("Equal difference.");
+		else
+			System.out.println("Unequal difference.");
+	}
 }
