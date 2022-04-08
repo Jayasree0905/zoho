bac36120ae5441f82f8d3f0fa0564ec27a18b5ee
Jayasree0905 <103204432+Jayasree0905@users.noreply.github.com>
2022-04-08 22:30:44 +0530

    Distinct words
    Print the distinct words in the given string.

--- Section_A_Question_7.java
+++ Section_A_Question_7.java
@@ -1,24 +1,29 @@
-/**
  * Print the distinct words in the given string.
  */
 import java.util.*;
+
 public class Section_A_Question_7 {
-    public static void main(String args[]) {
-    	isDistinctWord("This is Zoho and Zoho is good");
-    	isDistinctWord("we develop software we craft software");
-    }
-    public static void isDistinctWord(String input){
-       HashMap<String,String> hashMap = new LinkedHashMap<String,String>();
-        // To convert string to string array
-       String[] inputArray = input.split(" ");
-       //Iterate the string array and put the value to the hash map, hash map not allow duplicate key
-       for(String word : inputArray) {
-    	   hashMap.put(word,word);
-       }
-       //Print the value
-       for(HashMap.Entry<String,String> map:hashMap.entrySet()) {
-    	   System.out.print(map.getKey()+" ");
-       }
-         System.out.println();
-    }
+	public static void main(String args[]) {
+		System.out.println("Output 1:");
+		System.out.print("The distinct words are: ");
+		isDistinctWord("This is Zoho and Zoho is good");
+		System.out.println("Output 2:");
+		System.out.print("The distinct words are: ");
+		isDistinctWord("we develop software we craft software");
+	}
+
+	public static void isDistinctWord(String input) {
+		HashMap<String, String> hashMap = new LinkedHashMap<String, String>();
+		String[] inputArray = input.split(" "); // To convert string to string array
+		// Iterate the string array and put the value to the hash map, hash map not allow duplicate key
+		for (String word : inputArray) {
+			hashMap.put(word, word);
+		}
+		// Print the value
+		for (HashMap.Entry<String, String> map : hashMap.entrySet()) {
+
+			System.out.print(map.getKey() + " ");
+		}
+		System.out.println();
+	}
 }
